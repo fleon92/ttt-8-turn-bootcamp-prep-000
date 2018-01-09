11b7ce5d7f2eb968b18ad299145fd8af99106948
Felipe Leon <felipeleon92@gmail.com>
2018-01-09 02:22:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,19 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board,index)
+  if index.between?(0, 8) && position_taken? == false
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] = " " || board[index] = "" || board[index] = nil
+    false
+  else board[index] = "X" || board[index] = "O"
+    true
+  end
+end
